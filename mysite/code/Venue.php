62f855a4ed038b789a8ab67a7fb1ec4233fdda26
Ryan Kopf <ryankopf@dhcp80ff3187.dynamic.uiowa.edu>
2012-01-31 15:27:24 -0600

    Committing RSS stuff.

--- mysite/code/Venue.php
+++ mysite/code/Venue.php
@@ -6,13 +6,17 @@ class Venue extends Category {
 	public static $db = array(
 		"Information" => "HTMLText",
 		"Address" => "Varchar(255)",
-		"WebsiteURL" => "Text"
+		"WebsiteURL" => "Text",
+		"Lat" => "Varchar(255)",
+		"Lng" => "Varchar(255)"
 	);
 	function getCMSFields_forPopup() {
 		$fields = parent::getCMSFields_forPopup();
 		$fields->push( new SimpleHTMLEditorField( 'Information' ) );
 		$fields->push( new TextField('Address') );
 		$fields->push( new TextField('WebsiteURL','Website Address') );
+		$fields->push( new TextField('Lat') );
+		$fields->push( new TextField('Lng') );
 		return $fields;
 	}
 	
--- themes/afterclass/templates/Layout/AfterClassCategoryList.ss
+++ themes/afterclass/templates/Layout/AfterClassCategoryList.ss
@@ -15,6 +15,16 @@
 		<% if LinkURL %><p><a href="$LinkURL" target="_blank">Visit website</a></p><% end_if %>
 		<% if Address %><p><a href="#" target="_blank">Get Directions</a></p><% end_if %>
 		<!--<% control Events(2) %><div><% control Event %>$Title<% end_control %></div><% end_control %>-->
+		<ul class="category-list-events">
+			<% control Events %><li>
+			<% control Event %>
+			<img src="$Image.SmallImage.URL">
+			<div class="info">
+			$Title
+			</div>
+			<% end_control %>
+			</li><% end_control %>
+		</ul>
 		</li>
 	<% end_control %>
 </ul>
