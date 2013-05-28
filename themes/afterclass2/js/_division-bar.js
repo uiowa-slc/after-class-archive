b97b8be45c43d1dd59535798f705ae073b06ec02
Dustin Quam <quamsta@gmail.com>
2013-05-28 10:08:10 -0500

    fixed some jquery incompatibilities

--- themes/afterclass2/js/_division-bar.js
+++ themes/afterclass2/js/_division-bar.js
@@ -3,19 +3,19 @@ $(window).load(function() {
 
     $('.search-toggle').click(function() {
         $(this).toggleClass('active');
-        $('.division-search').slideToggle();
+      
         return false;
     });
 
 	$('.directory-toggle').click(function() {
 		$(this).toggleClass("active");
-		$('.division-directory').slideToggle();
+		
 		return false;
 	});
 
     $('.division-menu').bind('click', '.has-subnav a', function() {
         $(".division-show-hide").slideToggle();
-        $(this).toggleClass('active');
+        
 
     });
 
