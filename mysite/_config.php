01c150de43a2e3fcc6ef53355a089b25160a7466
Dustin Quam <quamsta@gmail.com>
2014-01-30 10:59:20 -0600

    upgrading preparations

---
+++ mysite/_config.php
@@ -0,0 +1,33 @@
+<?php
+
+global $project;
+$project = 'mysite';
+
+global $database;
+$database = 'afterclass';
+ 
+// Use _ss_environment.php file for configuration
+require_once("conf/ConfigureFromEnv.php");
+
+// This line set's the current theme. More themes can be
+// downloaded from http://www.silverstripe.org/themes/
+//SSViewer::set_theme('blackcandy');
+
+// Set the site locale
+i18n::set_locale('en_US');
+
+// enable nested URLs for this site (e.g. page/sub-page/)
+SiteTree::enable_nested_urls();
+//echo $_SERVER['REMOTE_ADDR'];
+
+HtmlEditorConfig::get('cms')->insertButtonsBefore(
+    'styleselect',
+    'removeformat'
+);
+
+
+/* Paste as Plain Text By Default */
+HtmlEditorConfig::get('cms')->setOption('paste_text_sticky',true);
+HtmlEditorConfig::get('cms')->setOption('paste_text_sticky_default',true);
+
+FulltextSearchable::enable();
