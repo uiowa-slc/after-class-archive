7a100d9422df1331f1dfaa4354fe4a8083a2df2e
Dustin Quam <quamsta@gmail.com>
2014-06-09 16:23:25 -0500

    slide mods to the orientation person model admin

--- mysite/code/OrientationPersonModelAdmin.php
+++ mysite/code/OrientationPersonModelAdmin.php
@@ -18,21 +18,24 @@ class OrientationPersonModelAdmin extends ModelAdmin {
 		  $context->getFields()->push(new CheckboxField('q[AddedToday]', 'Only people added today'));
 		  */
 		  
-		  $optionField = 
-		  	   new OptionsetField(
+		  $optionField = new OptionsetField(
 			   $name = "dateFilter",
-			   $title = "Select Time Range",
+			   $title = "Select a Time Range",
 			   $source = array(
-			   		  "q[AddedToday]" => "Added Today",
-				      "q[AddedYesterday]" => "Added Yesterday",
-				      "q[AddedThisYear]" => "Added This Year",
-				      "q[All]" => "All"
-					  ),
+		   		  "q[AddedToday]" => "Added Today",
+			      "q[AddedYesterday]" => "Added Yesterday",
+			      "q[AddedThisYear]" => "Added This Year",
+			      "q[All]" => "All"
+				),
 			   $value = "q[All]"
 			  );
-		   
-		   
+
+		  $dateField = new DateField('DateCreated', 'Or a Specific Date (overrides above)');
+		  $dateField->setConfig('showcalendar', true);
+
+		   $context->getFields()->removeByName("q[Created]");
 		   $context->getFields()->push($optionField);
+		   $context->getFields()->push($dateField);
 			
 	  }
 	  return $context; 
@@ -41,19 +44,29 @@ class OrientationPersonModelAdmin extends ModelAdmin {
   public function getList() {
 	  	$list = parent::getList();
 	  	$params = $this->request->requestVar('dateFilter');
+
+	  	$created = $this->request->requestVar('DateCreated');
+	  	$createdFormatted = date('Y-m-d', strtotime($created));
+
 	  	$firstofTheYear = date('Y').'-01-01';
 	  	$dayBefore = date('Y-m-d', strtotime("yesterday"));
-	  	//Debug::show($dayBefore);
+
 	  	$addedToday = date('Y-m-d');
-	  	if($this->modelClass == 'OrientationPerson' && ($params == 'q[AddedThisYear]')) {
-		  	$list = $list->filter(array('Created:GreaterThan' => $firstofTheYear));
-	  	}
-	  	if($this->modelClass == 'OrientationPerson' && ($params == 'q[AddedToday]')) {
-		  	$list = $list->filter(array('Created:GreaterThanOrEqual' => $addedToday));
-	  	}
-	  	if($this->modelClass == 'OrientationPerson' && ($params == 'q[AddedYesterday]')) {
-		  	$list = $list->filter(array('Created:PartialMatch' => $dayBefore));
-	  	}
+
+	  	if($this->modelClass == 'OrientationPerson' && isset($created) && $created) {
+	  		$list = $list->filter(array('Created:PartialMatch' => $createdFormatted));
+	  	}else{
+		  	if($this->modelClass == 'OrientationPerson' && ($params == 'q[AddedThisYear]')) {
+			  	$list = $list->filter(array('Created:GreaterThan' => $firstofTheYear));
+		  	}
+		  	if($this->modelClass == 'OrientationPerson' && ($params == 'q[AddedToday]')) {
+			  	$list = $list->filter(array('Created:GreaterThanOrEqual' => $addedToday));
+		  	}
+		  	if($this->modelClass == 'OrientationPerson' && ($params == 'q[AddedYesterday]')) {
+			  	$list = $list->filter(array('Created:PartialMatch' => $dayBefore));
+		  	}
+		}
+
 	  	return $list;
   }
 
