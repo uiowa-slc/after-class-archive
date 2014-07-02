5ffa0fd97edb69e1bfb72ab71f9f661ff8d73c2b
athaax <jonathansolis@dhcp80ff31ed.dynamic.uiowa.edu>
2014-07-02 12:06:24 -0500

    removed ac-json_events config

--- ac-json-events/_config.php
+++ ac-json-events/_config.php
@@ -2,4 +2,7 @@
 
 Object::add_extension('Page_Controller', 'JSONDisplayExtension');
 
+define('LOCALIST_BASE', 'http://events.nyp.org/');
+define('LOCALIST_FEED_URL', LOCALIST_BASE.'/api/2/');
+
 ?>
