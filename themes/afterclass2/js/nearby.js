81a4567e0bbc3264915f0d1eeb2f56ace10fdc6a
athaax <jonathansolis@dhcp80ff3115.dynamic.uiowa.edu>
2014-09-15 15:28:58 -0500

    Snappier maps, bugs fixed, limit events per bubble
    Reorganized and Refactored nearby.js

--- themes/afterclass2/js/nearby.js
+++ themes/afterclass2/js/nearby.js
@@ -1,14 +1,25 @@
+/* Global Variables */
 var markerArray = [];
 var infowindow = new google.maps.InfoWindow({
 	content: "holding...",
 	maxWidth: 340
 	});	
 var iowaCity = new google.maps.LatLng(41.661736, -91.540017);
-var	countVenue = 0;
+//var venueCount = $("#venuesWithEvents section").length;
+//var countVenue = 0;
 var venueFromMe = {};
-var venueCount = jQuery("#venuesWithEvents section").length;
-var geocoder = new google.maps.Geocoder();
+var userInitPosition;
 
+/* Helper Functions */
+
+//obsolete?
+function error(msg) {
+  var s = document.querySelector('#status');
+  s.innerHTML = typeof msg == 'string' ? msg : "failed";
+  s.className = 'fail';
+}
+
+//obsolete?
 function makeMarker(options){
    var pushPin = new google.maps.Marker({map:map});
    pushPin.setOptions(options);
@@ -20,32 +31,47 @@ function makeMarker(options){
    return pushPin;
 }
 
-function pinAndDist(initLocal, venueLatLng, venue) {
-	var venueDistance = google.maps.geometry.spherical.computeDistanceBetween(initLocal, venueLatLng);	
-	var venueID = venue.id;
-	var venueName = jQuery('#' + venueID).data("title");
-	var venueLink = jQuery('#' + venueID).data("link");
-		venueFromMe[venueID] = venueDistance;		
-	var marker = new google.maps.Marker({
-			position: venueLatLng,
-			map: map
-		});				    
+function handleNoGeolocation(errorFlag) {   	
+	var userInitPosition = iowaCity;
+    $('#status').text("Your location couldn't be detected. Showing events in Iowa City.");
+    return userInitPosition;
+}  
+
+/* End Helper Functions */
+
+function sortVenues() {
+	// empty list to sort venues by distance
+	var nearestVenues = [];
+	for (var venueID in venueFromMe) {
+		nearestVenues.push([venueID, venueFromMe[venueID]])
+	}	
+	nearestVenues.sort(function(a,b) {return a[1] - b[1]})
+	$("#venuesWithEvents .clear").remove();
+	for (var v=0; v < nearestVenues.length; v++) {
+		//console.log('sorting...');
+		var vid = nearestVenues[v];
+		$("#venuesWithEvents").append( $("#" + vid) );
+		$("#venuesWithEvents").append( $("<div class='clear'></div>") );
+	}
+}
+
+function addEventInfo( marker, venue ) {		
+	var venueName = $('#' + venue.id).data("title");
+	var venueLink = $('#' + venue.id).data("link");  
 	var eventsHere = [];
 	var eventsHereString = '';
 	var eventBubbleString = '';
-	var eventLimit = 6;
+	var eventLimit = 5;
 	
 	eventsHere.push("<a class='button tag' href='" + venueLink + "'>" + venueName + "</a>");
 	
-	jQuery(venue).children('div').each(function(index, Element) {
-		var eventTitle = jQuery(this).data('title');
-		var eventImage = jQuery(this).data('image');
-		var eventCancel = jQuery(this).data('cancel');
-		var eventLink = jQuery(this).data('link');
-		var eventCost = jQuery(this).data('cost');
-		var startDate = jQuery(this).data('startdate');
-		var startTime = jQuery(this).data('starttime');	
-
+	$(venue).children('div').each(function(index, Element) {
+		var eventTitle = $(this).data('title');
+		var eventImage = $(this).data('image');
+		var eventLink = $(this).data('link');
+		var eventCost = $(this).data('cost');
+		var startDate = $(this).data('startdate');
+		var startTime = $(this).data('starttime');	
 		var eventStringSeg = 
 		"<div> <h3> <a href='" + eventLink + "'>" + eventTitle + "</a> </h3> <ul class='infobox-list'>" + 
 			"<li>" + startDate + ", " + startTime + ((eventCost  !== "") ? ", Cost: " + eventCost : "") + "</li>" 
@@ -53,124 +79,136 @@ function pinAndDist(initLocal, venueLatLng, venue) {
 				
 		eventsHere.push(eventStringSeg);
 
+		// sets maximum number of events per bubble to eventLimit
 		if (index >= eventLimit) {
 			return false;
 		}
 	});
 	
+	/* string concatination for infoBubble */
 	eventsHereString = eventsHere.join(' ');
 	eventBubbleString = 
 		"<div class='event_bubble'>" +
 		eventsHereString +
 		"</div>";
 
+	/* make infoBubbles clickable */
     google.maps.event.addListener(marker, 'click', function () {
   		infowindow.setContent(eventBubbleString);
   		infowindow.open(map, this);	
   		//infowindow.maxWidth(200);
 	});
-	
-	//console.log('I have ' + venueName);
-
-	if(countVenue == venueCount) {
-		var nearestVenues = [];
-		for (var venueID in venueFromMe) {
-			nearestVenues.push([venueID, venueFromMe[venueID]])
-		}	
-		nearestVenues.sort(function(a,b) {return a[1] - b[1]})
-		for (var v=0; v < nearestVenues.length; v++) {
-			//console.log('sorting...');
-			var vid = nearestVenues[v];
-			jQuery("#venuesWithEvents").append( jQuery("#" + vid) );
-		}
-	}		
 }
 
-function venueGen(initLocal, callback) {	
-	jQuery('.venue').each(function(index, element) {
-		var title = jQuery(this).data("title");
-		var lat = jQuery(this).data("lat");
-		var lng = jQuery(this).data("lng");
-		var address = jQuery(this).data("address");
-		var venueLatLng;
-		var venueDistance;
+function venueGen() {	
+	//DON'T CHANGE IDs or Class Names in NearMePage.ss
+	//Scans venues loaded on page for data-attributes and pulls data
+
+	//note: geocoder used to be global variable
+	var geocoder = new google.maps.Geocoder();
+
+	$('.venue').each(function(index, element) {
 		var venue = this;
+		var venueID = venue.id;
+		var title = $(this).data("title");
+		var lat = $(this).data("lat");
+		var lng = $(this).data("lng");
+		var address = $(this).data("address");
+		var venueLatLng;
 		
 		if(lat && lng) {
-			countVenue = ++countVenue;
+			//console.log('venue has coords');
 			venueLatLng = new google.maps.LatLng(lat, lng);
-			//console.log('latyes');
-			callback(initLocal, venueLatLng, venue);
+		} else if (address != null) {
+			//console.log('venue does not have coords, has address');
+			geocoder.geocode( {'address': address}, function(results, status) {
+				if (status == google.maps.GeocoderStatus.OK) {
+				//Geocoder returns array of information, first indice is lat/lng
+				venueLatLng = results[0].geometry.location;
+				}				
+			});
 		} else {
-			//console.log('lat nO!' );
-			if (address != null) {
-				geocoder.geocode( { 'address': address}, function(results, status) {
-					countVenue = ++countVenue;
-					if (status == google.maps.GeocoderStatus.OK) {
-						venueLatLng = results[0].geometry.location;
-						callback(initLocal, venueLatLng, venue);
-					}				
-				});
-			}
-		}			
+			//console.log("No coords or address available for " + title)
+		}	
+
+		// drops pin
+		var marker = new google.maps.Marker({
+			position: venueLatLng,
+			map: map
+		});	
+
+		console.log("I have " + title + " at " + venueLatLng);
+		// fills 'infowindow' for each pin with list of events
+		addEventInfo( marker, venue );  
+
+		venueFromMe[venueID] = google.maps.geometry.spherical.computeDistanceBetween(userInitPosition, venueLatLng);
+
 	});	
+
+	/* when finished, sort venues divs on page. */
+	sortVenues();
+
 }
 
-function getInitLocal(callback) {
-	var initialLocation;
-	//DON'T CHANGE IDs in NearMePage.ss
+function getInitLocal() {
+	//finds users location
 	if(navigator.geolocation) {
 		//console.log("Browser DOES support Geolocation");
 	    var browserSupportFlag = true;
 	    navigator.geolocation.getCurrentPosition(function(position) {
-			initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-			//initialLocation = new google.maps.LatLng(41.664468, -91.535157)
-			var distanceFromInitialLocation = google.maps.geometry.spherical.computeDistanceBetween(initialLocation,iowaCity);	
+	    	//console.log('geolocated');
+			userInitPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+			var userDistanceFromIowaCity = google.maps.geometry.spherical.computeDistanceBetween(userInitPosition,iowaCity);	
 			// If the current position is too far away from Iowa City, just default to centering around Iowa City	
-			if (distanceFromInitialLocation < 32186.9) {
-				map.setCenter(initialLocation);
+			if (userDistanceFromIowaCity < 32186.9) {
+				map.setCenter(userInitPosition);
 				var image = 'themes/afterclass2/images/position-indicator.png';
 				var initalMarker = new google.maps.Marker({
-					position: initialLocation,
+					position: userInitPosition,
 					map: map,
 					icon: image
 				});  
-				initalMarker.setMap(map);
-				//$('#mapStart').prepend("Your location is indicated on the map with this icon: ");
-				//$('#mapStart').append("<img src='themes/afterclass2/images/position-indicator.png' />");
-				//$('#mapMark').prepend("To see nearby, upcoming events, touch a marker: ");
-				//$('#mapMark').append("<img src='themes/afterclass2/images/marker.png' />");
+				//initalMarker.setMap(map);
 			} else {
 				$('#mapLoaded').text("You're too far away from Iowa City. Here are events in Iowa City");
 			}
-			callback(initialLocation, pinAndDist);								  
-	    }, function() {
-	      handleNoGeolocation(browserSupportFlag);
+			venueGen();							  
+	    }, function(error) {
+	    	console.log('navigator failed');
+	    	var errorFlag = false;
+	    	userInitPosition = handleNoGeolocation(errorFlag)
+	    	map.setCenter(userInitPosition);
+	    	venueGen();
+	    }, { 
+	    	enableHighAccuracy: true, 
+	    	timeout: 1000,
+	    	maximumAge: 0
 	    });	     
 	} else {
-		// Browser does not support Geolocation
-		//console.log("Browser doesn't support Geolocation");
-	    browserSupportFlag = false;
-	    var initialLocation = handleNoGeolocation(browserSupportFlag);
+		console.log("Browser does NOT support Geolocation");
+	    var browserSupportFlag = false;
+	    userInitPosition = handleNoGeolocation(browserSupportFlag);
+	    venueGen();
 	}	
 }
 
-function genMap() {
-	// map generation
-	// Create an array of styles.
-		
-	var styledMap = new google.maps.StyledMapType(styles, {name: "Styled Map"});
+function genMapCanvas() {
+	// generates map styles, objects, DOM objects
+
     var mapcanvas = document.createElement('div');	
-		mapcanvas.id = 'mapcanvas';
-		mapcanvas.class = 'map-canvas';
-		mapcanvas.style.width = '100%';	
-	jQuery('.NearMePage .map-container').append(mapcanvas);
-	var nearMeMapOptions = {
+	 mapcanvas.id = 'mapcanvas';
+	 mapcanvas.class = 'map-canvas';
+	 mapcanvas.style.width = '100%';	
+	$('.NearMePage .map-container').append(mapcanvas);
+
+	//afterclassMap styles located in MapStyles.js
+	var afterclassMap = new google.maps.StyledMapType(styles, {name: "AfterClass Style Map"});
+	var MapOptions = {
 	    zoom: 15,
 	    center: iowaCity,
 	    panControl: false,
 	    mapTypeControl: false,
-	    //navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
+	    navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
 	    disableDefaultUI: false,
 	    mapTypeId: google.maps.MapTypeId.ROADMAP,
 	    streetViewControl: false,
@@ -178,29 +216,15 @@ function genMap() {
     		position: google.maps.ControlPosition.LEFT_BOTTOM
   		}
 	};
-	map = new google.maps.Map(document.getElementById("mapcanvas"), nearMeMapOptions);
-		map.mapTypes.set('map_style', styledMap);
-		map.setMapTypeId('map_style');
+	map = new google.maps.Map(document.getElementById("mapcanvas"), MapOptions);
+	 map.mapTypes.set('map_style', afterclassMap);
+	 map.setMapTypeId('map_style');
 	
-	getInitLocal(venueGen);
-	
-}
-
-function handleNoGeolocation(errorFlag) {   	
-	initialLocation = iowaCity;
-    map.setCenter(initialLocation);
-    $('#status').text("Your location couldn't be detected. Showing events in Iowa City.");
-    return initialLocation;
-}  
-
-function error(msg) {
-  var s = document.querySelector('#status');
-  s.innerHTML = typeof msg == 'string' ? msg : "failed";
-  s.className = 'fail';
+	//getInitLocal();
 }
 
 $(window).load(function() {
 	if( $('.NearMePage .map-container').length ){
-		genMap();
+		genMapCanvas( getInitLocal() );
 	}
 });
--- themes/afterclass2/templates/Layout/NearMePage.ss
+++ themes/afterclass2/templates/Layout/NearMePage.ss
@@ -1,37 +1,45 @@
-  <div class="row">
-    <div class="large-8 columns map-content">
-      <div class="map-container">
-      </div>
-      <% with $Calendar %>
-        <div class="map-jumplist">
-          <% if $ActiveVenueList %>
-          <p>See events at:
-            <% loop $ActiveVenueList %>
-                <a href = "#{$ID}" class="button tag">$Title.LimitCharacters(20)</a>
-            <% end_loop %>
-          </p>
-          <% else %>
-            <p>There aren't any events near you right now.</p>
-          <% end_if %>
-        </div>
-        <% loop $ActiveVenueList %>
-          <div class="event-card-list venue large" id="$ID" data-link="$Link" <% if $Latitude && $Longitude %> data-lat="$Latitude" data-lng="$Longitude" <% else %> data-address="$Address" <% end_if %> data-title="$Title.LimitCharacters(20)">    
-            <h2>$Title</h2>
-              <% loop $Events %>
-                <div data-title="$Title" data-link="$Link" data-image="$Image.URL" data-cancel="$CancelReason" data-cost="$Cost" <% if Sponsors %><% loop Sponsors %> data-sponsor="$Sponsors" <% end_loop %><% end_if %> 
-                  <% loop $Dates %> 
-                    data-startdate="$Format(l), $Format(F) $Format(j)" data-starttime="$Format(g):$Format(i) $Format(A)"
-                  <% end_loop %> "> 
-                <% include EventCard %> 
-                </div>  
-              <% end_loop %>
-          </div>
-          <div class="clear"></div>
-        <% end_loop %>
-      <% end_with %>
+<%--DON'T CHANGE IDs or Class Names in this page, otherwise nearby.js will break)--%>
+<div class="row">
+  <div class="large-8 columns map-content">
+
+    <!-- map dom space -->
+    <p id="status"></p>
+    <div class="map-container">
     </div>
 
-        <div class="large-4 columns">
-          <% include SecondaryNav %>
-        </div>  
+    <% with $Calendar %>
+
+      <div class="map-jumplist">
+        <% if $ActiveVenueList %>
+        <p>See events at:
+          <% loop $ActiveVenueList %>
+              <a href = "#{$ID}" class="button tag">$Title.LimitCharacters(20)</a>
+          <% end_loop %>
+        </p>
+        <% else %>
+          <p>There aren't any events near you right now.</p>
+        <% end_if %>
+      </div>
+
+      <div id="venuesWithEvents" >
+      <% loop $ActiveVenueList %>
+        <div class="event-card-list venue large" id="$ID" data-link="$Link" <% if $Latitude && $Longitude %> data-lat="$Latitude" data-lng="$Longitude" <% else %> data-address="$Address" <% end_if %> data-title="$Title.LimitCharacters(40)">    
+          <h2>$Title</h2>
+            <% loop $Events %>
+              <div data-title="$Title" data-link="$Link" data-image="$Image.URL" data-cost="$Cost" <% loop $Dates %> <% with $StartDateTime %> data-startdate="$Format(l), $Format(F) $Format(j)" data-starttime="$Format(g):$Format(i) $Format(A)" <% end_with %> <% end_loop %> "> 
+                <% include EventCard %> 
+              </div>  
+            <% end_loop %>
+        </div>
+        <div class="clear"></div>
+      <% end_loop %>
+      </div>
+      
+    <% end_with %>
+  </div>
+
+  <div class="large-4 columns">
+    <% include SecondaryNav %>
+  </div>  
+
 </div>
