342221995e9fc6e3bc0d5556e4852f30772ae0f6
Dustin Quam <quamsta@gmail.com>
2019-04-05 14:02:31 -0500

    remove extra fields from calendar page type

--- mysite/code/UiCalendarExtension.php
+++ mysite/code/UiCalendarExtension.php
@@ -21,8 +21,19 @@ use SilverStripe\ORM\DataExtension;
 
 		public function updateCMSFields(FieldList $fields){
 
-		}
 
+			$fields->removeByName('BackgroundImage');
+			$fields->removeByName('EventTypeFilterID');
+			$fields->removeByName('DepartmentFilterID');
+			$fields->removeByName('VenueFilterID');
+			$fields->removeByName('GeneralInterestFilterID');
+			$fields->removeByName('KeywordFilterID');
+			$fields->removeByName('LayoutType');
+			$fields->removeByName('Content');
+			$fields->removeByName('YoutubeBackgroundEmbed');
 
+			return $fields;
 
 	}
+
+}
