7243673364000922242eda2e690f35727bf607f2
Dustin Quam <quamsta@gmail.com>
2020-09-14 10:32:34 -0500

    admins can publish events

--- mysite/code/calendar/CalendarAddPageController.php
+++ mysite/code/calendar/CalendarAddPageController.php
@@ -4,6 +4,7 @@ use SilverStripe\Control\HTTPResponse;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\TextField;
 use SilverStripe\Forms\LabelField;
+use SilverStripe\Forms\CheckboxField;
 use SilverStripe\Forms\LiteralField;
 use SilverStripe\Forms\DateField;
 use SilverStripe\Forms\EmailField;
@@ -19,6 +20,8 @@ use SilverStripe\Security\Permission;
 use SilverStripe\Core\Injector\Injector;
 use Psr\Log\LoggerInterface;
 use SilverStripe\Control\Director;
+use SilverStripe\Security\PermissionProvider;
+
 
 class CalendarAddPageController extends PageController{
 
@@ -26,35 +29,40 @@ class CalendarAddPageController extends PageController{
 		'AddForm',
 	);
 
-	public function AddForm(){ 
-        $fields = new FieldList( 
+	public function AddForm(){
+        $fields = new FieldList(
             new TextField('SocialLink', 'Social media link:'),
             new LiteralField('SocialLinkInfo', '<label class="readonly">We currently support links from <i aria-hidden="true" class="fab fa-twitter"></i> Twitter and <i aria-hidden="true" class="fab fa-instagram"></i> Instagram </label>'),
             DateField::create('Expires', 'Expiry date (optional):'),
 
             new LabelField('ExpiresLabel', 'We\'ll show this post on After Class until the date above. Usually this would be the day after the event. If unsure, please leave this field blank.'),
             $emailField = new EmailField('SubmitterEmail', "Your email address (optional, only used if we need to clarify anything regarding the event).")
-        ); 
-        $actions = new FieldList( 
+        );
+
+        if (Permission::check('ADMIN')) {
+            $fields->push(CheckboxField::create('PublishNow', 'Publish this post immediately')->setDescription('This will skip the approval process. Use with care, please.'));
+        }
+
+        $actions = new FieldList(
             FormAction::create('submit', 'Submit your post')->addExtraClass('btn-lg')
-        ); 
+        );
 
         if ($member = Security::getCurrentUser()) {
-        	
+
         	$emailField->setValue($member->Email);
         }
-       
+
 		$form = new Form($this, 'AddForm', $fields, $actions);
 
 		//Disable captcha if logged in or in dev mode:
 		if (!($member) && (Director::isLive())) {
 		    $form->enableSpamProtection();
 		}
-         
-        return $form; 
+
+        return $form;
     }
 
-    public function submit($data, $form) { 
+    public function submit($data, $form) {
 
     	$link = $data['SocialLink'];
 
@@ -73,6 +81,8 @@ class CalendarAddPageController extends PageController{
 
 	    	}
 		}
+//publishRecursive
+
 
     	//Check for invalid link:
     	if (filter_var($link, FILTER_VALIDATE_URL) === false) {
@@ -87,7 +97,7 @@ class CalendarAddPageController extends PageController{
 		//Check for invalid domain in link:
 		$parsedLink = parse_url($link);
 		$validDomains = array('instagram.com', 'www.instagram.com', 'twitter.com', 'www.twitter.com');
-		
+
 		if(!in_array($parsedLink['host'], $validDomains)){
 
 	       	$data = new ArrayData(array(
@@ -115,12 +125,21 @@ class CalendarAddPageController extends PageController{
 
     	$newEvent->writeToStage('Stage');
 
+        $alertText = '<div class="alert alert-success" role="alert">'.$this->SubmissionThanks.'</div>';
+
+        if(isset($data['PublishNow'])){
+            if(($data['PublishNow'] == 1) && (Permission::check('ADMIN'))){
+                $newEvent->publishRecursive();
+                $alertText = '<div class="alert alert-success" role="alert">This post has <strong>saved and published</strong>. View here: <a style="color: #155724; text-decoration: underline;" href="'.$newEvent->AbsoluteLink().'">'.$newEvent->AbsoluteLink().'</a></div>';
+            }
+        }
+
     	//Injector::inst()->get(LoggerInterface::class)->debug('Query executed: ' . $sql);
     	// print_r($formData);
 
     	$this->sendNotificationEmail($data, $newEvent);
     	$data = new ArrayData(array(
-    			'Alert' => '<div class="alert alert-success" role="alert">'.$this->SubmissionThanks.'</div>',
+    			'Alert' => $alertText,
             	'Form' => ''
         ));
         return $this->customise($data)->render();
@@ -133,14 +152,17 @@ class CalendarAddPageController extends PageController{
     	$recipients = $this->EmailRecipients();
 
     	foreach($recipients as $recipient){
-			    	
-	    	$email = new Email(); 
-	         
-	        $email->setTo($recipient->EmailAddress); 
-	        $email->setReplyTo($newEvent->SubmitterEmail);
-	        $email->setSubject("[Social Calendar Submission] A link was submitted"); 
+
+	    	$email = new Email();
+
+	        $email->setTo($recipient->EmailAddress);
+            if($newEvent->SubmitterEmail){
+                 $email->setReplyTo($newEvent->SubmitterEmail);
+            }
+
+	        $email->setSubject("[Social Calendar Submission] A link was submitted");
 	         //TODO: Show some of the newly parsed link data in the email below:
-	        $messageBody = " 
+	        $messageBody = "
 	        	<p>The following link was submitted to the After Class submit-a-post form: </p>
 	            <p><strong>Social Media Link:</strong> <a href=\"{$data['SocialLink']}\">{$data['SocialLink']}</a></p>";
 
@@ -152,15 +174,15 @@ class CalendarAddPageController extends PageController{
 			}
 			$messageBody.="
 				<p><a href=\"admin/pages/edit/show/".$newEvent->ID."\">You can either publish or remove this post by editing this entry on After Class &rarr;</a>
-				</p>"; 
-	        $email->setBody($messageBody); 
+				</p>";
+	        $email->setBody($messageBody);
 	        // print_r($newEvent->SubmitterEmail);
 	        // print_r($newEvent->Expires);
 	        // print_r($messageBody);
-	        $email->send(); 
+	        $email->send();
     	}
     }
 
 
-	
+
 }
